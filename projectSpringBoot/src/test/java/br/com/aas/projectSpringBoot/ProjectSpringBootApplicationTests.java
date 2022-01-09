f6cd2928a165b4bbe000fb5124d7c22dbda32f38
aldreysilva <94716967+aldreysilva@users.noreply.github.com>
2022-01-08 21:28:57 -0300

    add project

---
+++ projectSpringBoot/src/test/java/br/com/aas/projectSpringBoot/ProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aas.projectSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
